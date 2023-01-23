83c8ed526f81a65e51a2e971287f88fad7bcab63
realitynotavailable <sopochkin.kirill@yandex.ru>
2023-01-23 15:22:03 +0300

    done

--- src/scripts/parts/main.js
+++ src/scripts/parts/main.js
@@ -1,2 +1,17 @@
 import "../../blocks/slider/slider";
-import "../../blocks/comment/comment-popup";
+import "../../blocks/popup/popup";
+
+let body = document.body;
+
+body.querySelector(".js-video").addEventListener("click", function(){
+    body.querySelector(".js-bg").hidden = true;
+    body.querySelector(".js-btn").hidden = true;
+    body.querySelector(".js-frame").src += "?autoplay=1&mute=0";
+})
+
+document.addEventListener("click", (e) => {
+    if(e.target.closest("[href]")) {
+        e.preventDefault();
+        console.log("dffsdfsf");
+    }
+});
